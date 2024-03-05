f1be5747cc75490b0ffd073108f8f60ef69e2427
daniel.szopa333 <daniel.szopa333@gmail.com>
2024-03-05 08:15:05 +0100

    Fix problem with email VO and add tests

--- src/Modules/Staff/Staff.Core/Domain/ValueObjects/Email.cs
+++ src/Modules/Staff/Staff.Core/Domain/ValueObjects/Email.cs
@@ -5,12 +5,13 @@ namespace Staff.Core.Domain.ValueObjects;
 
 public record Email
 {
-    private readonly Regex _regex = new Regex(@"^[a-z0-9]+\.?[a-z0-9]+@[a-z]+\.[a-z]{2,3}$", RegexOptions.IgnoreCase);
     public string Value { get; }
 
     public Email(string email)
     {
-        if (string.IsNullOrWhiteSpace(email) || _regex.IsMatch(email))
+        var regex = new Regex(@"^[a-z0-9]+\.?[a-z0-9]+@[a-z]+\.[a-z]{2,3}$", RegexOptions.IgnoreCase);
+
+        if (string.IsNullOrWhiteSpace(email) || !regex.IsMatch(email))
             throw new InvalidEmailException(email);
 
         Value = email;
---
+++ tests/Staff/Staff.UnitTests/ValueObjects/EmailTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Staff.Core.Domain.Exceptions;
+using Staff.Core.Domain.ValueObjects;
+
+namespace Staff.UnitTests.ValueObjects;
+
+public class EmailTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    internal void CreateEmail_WhenValueIsEmpty_ShouldThrowInvalidEmailException(string value)
+    {
+        Assert.Throws<InvalidEmailException>(() =>
+        {
+            new Email(value);
+        });
+    }
+
+    [Theory]
+    [InlineData("test@gmail.com")]
+    [InlineData("test1.23@GMAIL.COM")]
+    [InlineData("test.test2@gmail.com")]
+    [InlineData("0test012@onet.pl")]
+    internal void CreateEmail_WhenValueIsValid_ShouldCreateEmail(string value)
+    {
+        //act
+        var email = new Email(value);
+
+        //arrange
+        email.Value.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData(".test@gmail.com")]
+    [InlineData("test.@gmail.com")]
+    [InlineData("tes#@gmail.com")]
+    [InlineData("tes#@gmail.cm")]
+    [InlineData("tes#@gmail.comm")]
+    [InlineData("tes#@gmail.c")]
+    [InlineData("test@gmail,c")]
+    internal void CreateEmail_WhenValueIsInValid_ShouldThrowInvalidEmailException(string value)
+    {
+        Assert.Throws<InvalidEmailException>(() =>
+        {
+            new Email(value);
+        });
+    }
+}
