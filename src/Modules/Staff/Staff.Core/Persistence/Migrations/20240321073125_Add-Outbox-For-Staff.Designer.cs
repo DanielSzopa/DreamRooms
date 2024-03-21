4aaf8d11389921f0a092afc17fe4bf38a03aed71
DanielSzopa <daniel.szopa333@gmail.com>
2024-03-21 08:32:24 +0100

    Create migration for outbox for Staff module

--- src/Modules/Staff/Staff.Core/Persistence/Migrations/20240321073125_Add-Outbox-For-Staff.Designer.cs
+++ src/Modules/Staff/Staff.Core/Persistence/Migrations/20240321073125_Add-Outbox-For-Staff.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Staff.Core.Persistence;
 
@@ -11,9 +12,11 @@ using Staff.Core.Persistence;
 namespace Staff.Core.Persistence.Migrations
 {
     [DbContext(typeof(StaffDbContext))]
-    partial class StaffDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321073125_Add-Outbox-For-Staff")]
+    partial class AddOutboxForStaff
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Staff/Staff.Core/Persistence/Migrations/20240321073125_Add-Outbox-For-Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Staff.Core.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOutboxForStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                schema: "staff",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TraceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProcessedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxMessages",
+                schema: "staff");
+        }
+    }
+}
